e406dc9338924f4537fe3a1eca074a101c24740c
will1184 <brandonwilliammg@gmail.com>
2022-11-24 09:15:58 -0600

    3rd, commit

--- app/src/main/java/com/android/navigation/FragmentConfirmation.kt
+++ app/src/main/java/com/android/navigation/FragmentConfirmation.kt
@@ -1,20 +1,25 @@
 package com.android.navigation
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.navArgs
+import kotlinx.android.synthetic.main.fragment_confirmation.*
+import kotlinx.android.synthetic.main.fragment_confirmation.view.*
+import kotlinx.android.synthetic.main.fragment_specify_amount.*
+import org.w3c.dom.Text
 
 
 /**
  * A simple [Fragment] subclass.
- * Use the [FragmentConfirmation.newInstance] factory method to
  * create an instance of this fragment.
  */
 class FragmentConfirmation : Fragment() {
 
-
+    val args:FragmentConfirmationArgs by navArgs()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
@@ -24,7 +29,13 @@ class FragmentConfirmation : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_confirmation, container, false)
+
+        val view=inflater.inflate(R.layout.fragment_confirmation, container, false)
+
+        val textMessage=args.name
+        view.text_confirmation.text= textMessage
+        return view
+
     }
 
 
--- app/src/main/java/com/android/navigation/FragmentSpecifyAmount.kt
+++ app/src/main/java/com/android/navigation/FragmentSpecifyAmount.kt
@@ -5,7 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.navigation.Navigation
+import androidx.navigation.findNavController
 import kotlinx.android.synthetic.main.fragment_choose_recipient.*
 import kotlinx.android.synthetic.main.fragment_main.*
 import kotlinx.android.synthetic.main.fragment_specify_amount.*
@@ -24,6 +26,7 @@ class FragmentSpecifyAmount : Fragment() {
 
     }
 
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -35,7 +38,15 @@ class FragmentSpecifyAmount : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         btn_send.setOnClickListener{
-            Navigation.findNavController(view).navigate(R.id.action_fragmentSpecifyAmount_to_fragmentConfirmation)
+            val message=text_amount.text.toString()
+            if(message.isNotEmpty()){
+
+                val action = FragmentSpecifyAmountDirections.actionFragmentSpecifyAmountToFragmentConfirmation(message)
+                Navigation.findNavController(view).navigate(action)
+            }else{
+                Toast.makeText(activity,"Message no enviado",Toast.LENGTH_SHORT).show()
+            }
+
         }
         btn_cancel.setOnClickListener{
             Navigation.findNavController(view).navigate(R.id.action_fragmentSpecifyAmount_to_fragmentChooseRecipient)
